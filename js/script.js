a04a157147b61e32563cfe370a43ef1f2df422dd
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2022-08-01 17:23:07 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  //console.log(subType);
+  updateSubscriptionDiv();
+});
+
+subDurationElement.addEventListener("change", function (e) {
+  subType = Number(e.target.value);
+  //console.log(subDuration);
+  updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5; //cost of basic plan
+  if (subType === "standard") {
+    monthlyCost = 7;
+  }
+  if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
